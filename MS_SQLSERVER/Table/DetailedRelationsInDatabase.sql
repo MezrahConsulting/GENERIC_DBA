09a0e6ddb5672510adc3ca949413de231d2b06a1
Dave Babler (Mezrah DBA) <dbabler@mezrahconsulting.com>
2020-08-05 15:26:18 -0400

    update to table aliases for readability

--- MS_SQLSERVER/Table/DetailedRelationsInDatabase.sql
+++ MS_SQLSERVER/Table/DetailedRelationsInDatabase.sql
@@ -3,24 +3,24 @@ including the type of relation.
 --Dave Babler
 */
 
-SELECT schema_name(fk_tab.schema_id) + '.' + fk_tab.name AS foreign_table,
+SELECT schema_name(FK_TAB.schema_id) + '.' + FK_TAB.name AS foreign_table,
     '>-' AS rel,
-    schema_name(pk_tab.schema_id) + '.' + pk_tab.name AS primary_table,
+    schema_name(PK_TAB.schema_id) + '.' + PK_TAB.name AS primary_table,
     SUBSTRING(column_names, 1, len(column_names)-1) AS [fk_columns],
-    fk.name AS fk_constraint_name
-FROM sys.foreign_keys fk
-    INNER JOIN sys.tables fk_tab
-        ON fk_tab.object_id = fk.parent_object_id
-    INNER JOIN sys.tables pk_tab
-        ON pk_tab.object_id = fk.referenced_object_id
-    CROSS APPLY (SELECT col.[name] + ', '
-                    FROM sys.foreign_key_columns fk_c
-                        INNER JOIN sys.columns col
-                            ON fk_c.parent_object_id = col.object_id
-                            AND fk_c.parent_column_id = col.column_id
-                    WHERE fk_c.parent_object_id = fk_tab.object_id
-                      AND fk_c.constraint_object_id = fk.object_id
-                            ORDER BY col.column_id
+    FK.name AS fk_constraint_name
+FROM sys.foreign_keys FK
+    INNER JOIN sys.tables FK_TAB
+        ON FK_TAB.object_id = FK.parent_object_id
+    INNER JOIN sys.tables PK_TAB
+        ON PK_TAB.object_id = FK.referenced_object_id
+    CROSS APPLY (SELECT COL.[name] + ', '
+                    FROM sys.foreign_key_columns FK_C
+                        INNER JOIN sys.columns COL
+                            ON FK_C.parent_object_id = COL.object_id
+                            AND FK_C.parent_column_id = COL.column_id
+                    WHERE FK_C.parent_object_id = FK_TAB.object_id
+                      AND FK_C.constraint_object_id = FK.object_id
+                            ORDER BY COL.column_id
                             for xml path ('') ) D (column_names)
-ORDER BY schema_name(fk_tab.schema_id) + '.' + fk_tab.name,
-    schema_name(pk_tab.schema_id) + '.' + pk_tab.name
+ORDER BY schema_name(FK_TAB.schema_id) + '.' + FK_TAB.name,
+    schema_name(PK_TAB.schema_id) + '.' + PK_TAB.name
