e3485acbf84656368094e1699187cb4ddadc9432
Dave Babler (Mezrah DBA) <dbabler@mezrahconsulting.com>
2020-08-11 13:27:47 -0400

    add SEVERE warning to Rebuild Index Script

--- MS_SQLSERVER/Index/RebuildIndexScript.sql
+++ MS_SQLSERVER/Index/RebuildIndexScript.sql
@@ -1,3 +1,15 @@
+/**warning
+* never
+* ever
+* ever
+* ever
+* ever
+* ever
+* use this on production without first on development and then test your hardest queries on the database before 
+* you approve for production --Dave Babler
+*/
+
+
 SELECT  OBJECT_NAME(ind.OBJECT_ID) AS TableName 
         , ind.name AS IndexName, indexstats.index_type_desc AS IndexType
         , indexstats.avg_fragmentation_in_percent
