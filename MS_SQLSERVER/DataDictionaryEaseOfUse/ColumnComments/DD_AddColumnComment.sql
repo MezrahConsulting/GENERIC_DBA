88a2f9f8adc2cf90c8a52b9b5facc8135f9a580b
Dave Babler <dbabler@mezrahconsulting.com>
2020-08-26 09:50:46 -0400

    prior to compiling add column comments into procedure begin show column comment

--- MS_SQLSERVER/DataDictionaryEaseOfUse/ColumnComments/DD_AddColumnComment.sql
+++ MS_SQLSERVER/DataDictionaryEaseOfUse/ColumnComments/DD_AddColumnComment.sql
@@ -1,46 +1,46 @@
-	-- Add the parameters for the stored procedure here
-	
-DECLARE    @strTableName NVARCHAR(64), 
-	@strComment NVARCHAR(360);
-
+-- Add the parameters for the stored procedure here
+DECLARE @strTableName NVARCHAR(64)
+        , @strColumnName NVARCHAR(64)
+        , @strComment NVARCHAR(360);
 /**Note: vrt is for Variant, which is the absurd way SQL Server stores it's Strings in the data dictionary
 * supposedly for 'security' --Dave Babler*/
+DECLARE @vrtComment SQL_VARIANT;
 
-DECLARE @vrtComment SQL_Variant;
+SET @vrtComment = CAST(@strComment AS SQL_VARIANT);
 
-SET @vrtComment = CAST(@strComment AS SQL_Variant);
-BEGIN TRY 
+BEGIN TRY
 	SET NOCOUNT ON;
-    		IF NOT EXISTS (
-				SELECT NULL
-				FROM SYS.EXTENDED_PROPERTIES
-				WHERE [major_id] = OBJECT_ID(@strTableName)
-					AND [name] = N'MS_Description'
-					AND [minor_id] = (
-						SELECT [column_id]
-						FROM SYS.COLUMNS
-						WHERE [name] = @strColumnName
-							AND [object_id] = OBJECT_ID(@strTableName)
-						)
-				)
-			EXECUTE sp_addextendedproperty @name = N'MS_Description'
-				, @value = @vrtComment
-				, @level0type = N'SCHEMA'
-				, @level0name = N'dbo'
-				, @level1type = N'TABLE'
-				, @level1name = @strTableName
-				, @level2type = N'COLUMN'
-				, @level2name = @strColumnName;
-		ELSE
-			EXECUTE sp_updateextendedproperty @name = N'MS_Description'
-				, @value = @vrtComment
-				, @level0type = N'SCHEMA'
-				, @level0name = N'dbo'
-				, @level1type = N'TABLE'
-				, @level1name = @strTableName
-				, @level2type = N'COLUMN'
-				, @level2name = @strColumnName;
-        END TRY
+
+	IF NOT EXISTS (
+			SELECT NULL
+			FROM SYS.EXTENDED_PROPERTIES
+			WHERE [major_id] = OBJECT_ID(@strTableName)
+				AND [name] = N'MS_Description'
+				AND [minor_id] = (
+					SELECT [column_id]
+					FROM SYS.COLUMNS
+					WHERE [name] = @strColumnName
+						AND [object_id] = OBJECT_ID(@strTableName)
+					)
+			)
+		EXECUTE sp_addextendedproperty @name = N'MS_Description'
+			, @value = @vrtComment
+			, @level0type = N'SCHEMA'
+			, @level0name = N'dbo'
+			, @level1type = N'TABLE'
+			, @level1name = @strTableName
+			, @level2type = N'COLUMN'
+			, @level2name = @strColumnName;
+	ELSE
+		EXECUTE sp_updateextendedproperty @name = N'MS_Description'
+			, @value = @vrtComment
+			, @level0type = N'SCHEMA'
+			, @level0name = N'dbo'
+			, @level1type = N'TABLE'
+			, @level1name = @strTableName
+			, @level2type = N'COLUMN'
+			, @level2name = @strColumnName;
+END TRY
 
 BEGIN CATCH
 	INSERT INTO dbo.DB_EXCEPTION_TANK
