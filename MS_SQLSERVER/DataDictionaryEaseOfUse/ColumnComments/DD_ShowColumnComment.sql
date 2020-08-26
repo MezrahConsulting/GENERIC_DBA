1e714b9d6c3cc697766b10bfd9e186da567abba8
Dave Babler <dbabler@mezrahconsulting.com>
2020-08-26 12:55:00 -0400

    ADD more aggressive error checking to ShowColumnComments Did not add more exception handling just error checking

--- MS_SQLSERVER/DataDictionaryEaseOfUse/ColumnComments/DD_ShowColumnComment.sql
+++ MS_SQLSERVER/DataDictionaryEaseOfUse/ColumnComments/DD_ShowColumnComment.sql
@@ -1,53 +1,71 @@
 DECLARE @strTableName NVARCHAR(64)
-	, @strColumnName NVARCHAR(64);
+    , @strColumnName NVARCHAR(64);
 DECLARE @strMessageOut NVARCHAR(320);
 
 BEGIN TRY
-	IF EXISTS (
-			SELECT NULL
-			FROM SYS.EXTENDED_PROPERTIES
-			WHERE [major_id] = OBJECT_ID(@strTableName)
-				AND [name] = N'MS_Description'
-				AND [minor_id] = (
-					SELECT [column_id]
-					FROM SYS.COLUMNS
-					WHERE [name] = @strColumnName
-						AND [object_id] = OBJECT_ID(@strTableName)
-					)
-			)
-	BEGIN
-		SELECT TOP 1 @strMessageOut = CAST(ep.value AS  NVARCHAR(320))
-		FROM sys.extended_properties ep
-		INNER JOIN sys.all_objects ob
-			ON ep.major_id = ob.object_id
-		INNER JOIN sys.tables AS st
-			ON ob.object_id = st.object_id
-		INNER JOIN sys.columns AS c
-			ON ep.major_id = c.object_id
-				AND ep.minor_id = c.column_id
-		WHERE st.name = @strTableName
-			AND c.name = @strColumnName
-	END
-	ELSE
-	BEGIN
-		SET @strMessageOut = @strTableName + ' ' + @strColumnName + 
-			N' currently has no comments please use DD_AddColumnComment to add a comment!';
-	END
 
-	SELECT @strColumnName AS 'ColumnName'
-		, @strMessageOut AS 'TableComment';
+    IF EXISTS (
+            /**Check to see if the column or table actually exists -- Babler*/
+            SELECT 1
+            FROM INFORMATION_SCHEMA.COLUMNS
+            WHERE TABLE_NAME = @strTableName
+                AND COLUMN_NAME = @strColumnName
+        )
+        BEGIN 
+            IF EXISTS (
+                    SELECT NULL
+                    FROM SYS.EXTENDED_PROPERTIES
+                    WHERE [major_id] = OBJECT_ID(@strTableName)
+                        AND [name] = N'MS_Description'
+                        AND [minor_id] = (
+                            SELECT [column_id]
+                            FROM SYS.COLUMNS
+                            WHERE [name] = @strColumnName
+                                AND [object_id] = OBJECT_ID(@strTableName)
+                            )
+                    )
+            BEGIN
+                SELECT TOP 1 @strMessageOut = CAST(ep.value AS  NVARCHAR(320))
+                FROM sys.extended_properties ep
+                INNER JOIN sys.all_objects ob
+                    ON ep.major_id = ob.object_id
+                INNER JOIN sys.tables AS st
+                    ON ob.object_id = st.object_id
+                INNER JOIN sys.columns AS c	
+                    ON ep.major_id = c.object_id
+                        AND ep.minor_id = c.column_id
+                WHERE st.name = @strTableName
+                    AND c.name = @strColumnName
+            END
+            ELSE
+            BEGIN
+                SET @strMessageOut = @strTableName + ' ' + @strColumnName + 
+                    N' currently has no comments please use DD_AddColumnComment to add a comment!';
+            END
+
+            SELECT @strColumnName AS 'ColumnName'
+                , @strMessageOut AS 'TableComment';
+        END
+    ELSE 
+        BEGIN
+         SET @strMessageOut = 'Either the column you typed in: ' + @strColumnName + ' or, '
+                            + ' the table you typed in: ' + @strTableName + ' '
+                            + 'is invalid, check spelling, try again? ';
+         SELECT @strMessageOut AS 'NON_LOGGED_ERROR_MESSAGE'
+        END
+
 END TRY
 
 BEGIN CATCH
-	INSERT INTO dbo.DB_EXCEPTION_TANK
-	VALUES (
-		SUSER_SNAME()
-		, ERROR_NUMBER()
-		, ERROR_STATE()
-		, ERROR_SEVERITY()
-		, ERROR_PROCEDURE()
-		, ERROR_LINE()
-		, ERROR_MESSAGE()
-		, GETDATE()
-		);
+    INSERT INTO dbo.DB_EXCEPTION_TANK
+    VALUES (
+        SUSER_SNAME()
+        , ERROR_NUMBER()
+        , ERROR_STATE()
+        , ERROR_SEVERITY()
+        , ERROR_PROCEDURE()
+        , ERROR_LINE()
+        , ERROR_MESSAGE()
+        , GETDATE()
+        );
 END CATCH;
