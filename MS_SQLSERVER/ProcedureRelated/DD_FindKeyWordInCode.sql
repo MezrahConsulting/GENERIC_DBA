21011b43cd8d2c384dd23964085e4d5370f413ab
Dave Babler <dbabler@mezrahconsulting.com>
2020-09-17 12:50:45 -0400

    finish work on find keyword in code

--- MS_SQLSERVER/ProcedureRelated/DD_FindKeyWordInCode.sql
+++ MS_SQLSERVER/ProcedureRelated/DD_FindKeyWordInCode.sql
@@ -9,7 +9,7 @@
 CREATE OR ALTER PROCEDURE DD_FindKeyWordInCode
 	-- Add the parameters for the stored procedure here
 	@strKeyWord NVARCHAR(MAX)
-	, @dlistTypeOfCodeToSearch VARCHAR(16) = NULL
+	, @dlistTypeOfCodeToSearch VARCHAR(40) = NULL
 AS
 BEGIN TRY
 SET NOCOUNT ON;
@@ -17,60 +17,57 @@ SET NOCOUNT ON;
 DECLARE @charComma CHAR(1) = ',' -- I did not want to deal with yet another escape sequence 
 	, @TSQLParameterDefinitions NVARCHAR(800)
 	, @strKeyWordPrepared NVARCHAR(MAX)
-	, @dlistTypeOfCodeToSearch VARCHAR(40)
 	, @sqlSearchFinal NVARCHAR(MAX) = NULL;
+	
+SET @strKeyWordPrepared =   '%' + @strKeyWord + '%'; --gotta add those % sinces for dynamic sql LIKE statments outside of the statement
 
-	SET @sqlSearchFinal = 
-		N'SELECT DISTINCT o.name
-   	, o.[type]
-   	, o.type_desc
-   	, m.DEFINITION
-   FROM sys.sql_modules m
-   INNER JOIN sys.objects o
-   	ON m.object_id = o.object_id
-   WHERE m.DEFINITION LIKE ''%'' @strKeyWord_ph ''%'''
-		;
 
 	IF @dlistTypeOfCodeToSearch IS NOT NULL
 		BEGIN
-			SET @TSQLParameterDefinitions = N'@strKeyWord_ph NVARCHAR(MAX)
-												, @dlistTypeOfCodeToSearch_ph NVARCHAR(24)
-												, @charComma_ph CHAR(1)';
-					SET @sqlSearchFinal = 
-										N'SELECT DISTINCT o.name
-												, o.[type]
-												, o.type_desc
-												, m.DEFINITION
-											FROM sys.sql_modules m
-										  	INNER JOIN UTL_fn_CSVToSingleColumnTable(@dlistTypeOfCodeToSearch_ph
-											  										 , @charComma_ph) AS Q 
-   														ON o.[type] = Q.StringValue COLLATE Latin1_General_CI_AS_KS_WS
-											INNER JOIN sys.objects o
-												ON m.object_id = o.object_id
-											WHERE m.DEFINITION LIKE ''%'' @strKeyWord_ph ''%'''	;
-					EXEC sp_executesql @sqlSearchFinal
-										, @TSQLParameterDefinitions
-										, @strKeyWord_ph = @strKeyWord
-										, @dlistTypeOfCodeToSearch_ph = @dlistTypeOfCodeToSearch
-										, @charComma_ph = @charComma;
-		END
-	ELSE
-		BEGIN
-			SET @TSQLParameterDefinitions = N'@strKeyWord_ph NVARCHAR(MAX)';
+		/**We join the table valued function to the DD to get the types of functions we want -- Babler */
 			SET @sqlSearchFinal = 
-											N'SELECT DISTINCT o.name
+								   N'SELECT DISTINCT o.name
 													, o.[type]
 													, o.type_desc
 													, m.DEFINITION
 												FROM sys.sql_modules m
 												INNER JOIN sys.objects o
 													ON m.object_id = o.object_id
-												WHERE m.DEFINITION LIKE ''%'' @strKeyWord_ph ''%''
-												ORDER BY o.[type]';
+												INNER JOIN UTL_fn_CSVToSingleColumnTable(@dlistTypeOfCodeToSearch_ph, @charComma_ph) AS Q
+													ON o.[type] = Q.StringValue COLLATE Latin1_General_CI_AS_KS_WS
+												WHERE m.DEFINITION LIKE @strKeyWord_ph
+												ORDER BY o.[type] ';
+			SET @TSQLParameterDefinitions = N'@strKeyWord_ph NVARCHAR(MAX)
+												, @dlistTypeOfCodeToSearch_ph NVARCHAR(24)
+												, @charComma_ph CHAR(1)';
 			EXEC sp_executesql @sqlSearchFinal
 								, @TSQLParameterDefinitions
-								, @strKeyWord_ph = @strKeyWord;
+								, @strKeyWord_ph = @strKeyWordPrepared
+								, @dlistTypeOfCodeToSearch_ph = @dlistTypeOfCodeToSearch
+								, @charComma_ph = @charComma;
 		END
+	ELSE
+		BEGIN
+			SET @sqlSearchFinal = 
+									N'SELECT DISTINCT o.name
+													, o.[type]
+													, o.type_desc
+													, m.DEFINITION
+												FROM sys.sql_modules m
+												INNER JOIN sys.objects o
+													ON m.object_id = o.object_id
+												WHERE m.DEFINITION LIKE @strKeyWord_ph
+												ORDER BY o.[type] ' ;
+
+				SET @TSQLParameterDefinitions = N'@strKeyWord_ph NVARCHAR(MAX)';
+				EXEC sp_executesql @sqlSearchFinal
+									, @TSQLParameterDefinitions
+									, @strKeyWord_ph = @strKeyWordPrepared;
+		END
+		
+
+
+
 END TRY
 
 BEGIN CATCH
